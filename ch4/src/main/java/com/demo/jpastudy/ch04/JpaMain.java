efa982136f35ece1d62b06da491280179cf8ee55
demo.lim <demo.lim@kakaocorp.com>
2021-01-27 22:47:33 +0900

    ch4 모듈 생성

---
+++ ch4/src/main/java/com/demo/jpastudy/ch04/JpaMain.java
@@ -0,0 +1,62 @@
+package com.demo.jpastudy.ch04;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+  public static void main(String[] args) {
+
+    // 엔티티 매니저 팩토리 생성
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+
+    // 엔티티 매니저 생성
+    EntityManager em = emf.createEntityManager();
+
+    // 트랜잭션 획득
+    EntityTransaction tx = em.getTransaction();
+
+    try{
+      tx.begin();
+      logic(em);
+      tx.commit();
+    } catch (Exception e){
+      tx.rollback();
+    }finally {
+      em.close();
+    }
+    emf.close();
+  }
+
+  private static void logic(EntityManager em){
+
+    String id = "id1";
+    Member member = new Member();
+    member.setId(id);
+    member.setUsername("demo");
+    member.setAge(30);
+
+    // 등록
+    em.persist(member);
+
+    // 수정
+    member.setAge(31);
+
+    // 한건 조회
+    Member findMember = em.find(Member.class, id);
+    System.out.println("findMember=" + findMember.getUsername()
+        + ", age=" + findMember.getAge());
+
+    // 목록 조회
+    List<Member> members =
+        em.createQuery("select m from Member m", Member.class)
+        .getResultList();
+    System.out.println("member.size=" + members.size());
+
+    // 삭제
+    em.remove(members);
+  }
+}
