48e00850a8a5a396c4073cd4e7a37316322b300a
vljh246v <vljh246v@naver.com>
2021-01-28 23:44:41 +0900

    TableGenerator 방식으로 예제 변경

--- ch4/src/main/java/com/demo/jpastudy/ch04/Board.java
+++ ch4/src/main/java/com/demo/jpastudy/ch04/Board.java
@@ -5,16 +5,23 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
 
 @Entity
-@SequenceGenerator(
+//@SequenceGenerator(
+//    name = "BOARD_SEQ_GENERATOR",
+//    sequenceName = "BOARD_SEQ",
+//    initialValue = 1, allocationSize = 1)
+@TableGenerator(
     name = "BOARD_SEQ_GENERATOR",
-    sequenceName = "BOARD_SEQ",
-    initialValue = 1, allocationSize = 1)
+    table = "MY_SEQUENCES",
+    pkColumnValue = "BOARD_SEQ", allocationSize = 1
+)
 public class Board {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
+  @GeneratedValue(strategy = GenerationType.TABLE, generator = "BOARD_SEQ_GENERATOR")
   private Long id;
 
   public Long getId() {
