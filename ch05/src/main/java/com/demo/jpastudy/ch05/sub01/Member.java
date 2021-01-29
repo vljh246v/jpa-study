23968858719b4e380b022eb050fc83871f77a7bf
vljh246v <vljh246v@naver.com>
2021-01-30 02:29:41 +0900

    ch05 @Entity 작성 및 연관관계 표시

--- ch05/src/main/java/com/demo/jpastudy/ch05/sub01/Member.java
+++ ch05/src/main/java/com/demo/jpastudy/ch05/sub01/Member.java
@@ -1,11 +1,22 @@
 package com.demo.jpastudy.ch05.sub01;
 
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Member {
 
+  @Id
+  @Column(name = "MEMBER_ID")
   private String id;
   private String username;
 
+  @ManyToOne
+  @JoinColumn(name = "TEAM_ID")
   private Team team;
 
   public Member(String id, String username){
--- ch05/src/main/java/com/demo/jpastudy/ch05/sub01/Team.java
+++ ch05/src/main/java/com/demo/jpastudy/ch05/sub01/Team.java
@@ -1,7 +1,17 @@
 package com.demo.jpastudy.ch05.sub01;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Team {
+
+  @Id
+  @Column(name = "TEAM_ID")
   private String id;
+
   private String name;
 
   public Team(String id, String name){
