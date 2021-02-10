07a54fbdee85f7ce92c4f6e08f89b25d4cb1303c
vljh246v <vljh246v@naver.com>
2021-02-11 03:13:10 +0900

    1:N 양방향 정리 엔티티 작성

--- ch06/src/main/java/com/demo/jpastudy/ch06/Member.java
+++ ch06/src/main/java/com/demo/jpastudy/ch06/Member.java
@@ -5,36 +5,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "MEMBER_ID")
   private Long id;
+
   private String username;
+
+  @ManyToOne
+  @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+  private Team team;
 
   public Member(String username) {
     this.username = username;
   }
 
   public Member() {
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
   }
 }
