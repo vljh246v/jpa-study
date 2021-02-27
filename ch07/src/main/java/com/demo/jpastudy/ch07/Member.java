1300c3bc5b7b155229194d7e8ce7f89b29689a5c
vljh246v <vljh246v@naver.com>
2021-02-27 22:41:13 +0900

    @AttributeOverrides, @AttributeOverride 예제 작성

--- ch07/src/main/java/com/demo/jpastudy/ch07/Member.java
+++ ch07/src/main/java/com/demo/jpastudy/ch07/Member.java
@@ -1,6 +1,9 @@
 package com.demo.jpastudy.ch07;
 
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,10 @@ import lombok.Setter;
 @Getter @Setter
 @NoArgsConstructor
 @Entity
+@AttributeOverrides({
+    @AttributeOverride(name = "id", column = @Column(name = "MEMBER_ID")),
+    @AttributeOverride(name = "name", column = @Column(name = "MEMBER_NAME"))
+})
 public class Member extends BaseEntity{
 
   private String email;
